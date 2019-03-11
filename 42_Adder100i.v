40806341a3cb8086dd21e77a6ed44abda1254c07
xiaopi <382215239@qq.com>
2019-03-11 09:08:26 +0800

    添加新方法
    M-HHH提供的更为精简的方法。
    
    Co-Authored-By: M <mmmmmmjjjh@hotmail.com>

--- 42_Adder100i.v
+++ 42_Adder100i.v
@@ -69,4 +69,26 @@ module top_module(
         end
     end
 
+    // M-HHH 提供的精简思路
+    // assign cout[0] = a[0] & b[0] | a[0] & cin | b[0] & cin;
+    // assign sum[0]  = a[0] ^ b[0] ^ cin;
+    
+    // integer i, j;
+    
+    // always @ (*)
+    //     begin
+    //         for (i=1; i<100; i++)    
+    //             begin
+    //                 sum[i]  = a[i] ^ b[i] ^ cout[i-1];
+    //             end
+    //     end
+    
+    // always @ (*)
+    //     begin
+    //         for(i=1; i<100; i++)
+    //             begin
+    //                 cout[i] = a[i] & b[i] | a[i] & cout[i-1] | b[i] & cout[i-1];  
+    //             end
+    //     end
+
 endmodule
