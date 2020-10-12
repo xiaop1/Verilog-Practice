7cd140ac4521deb5ed56c06436d31f7641ae904e
xiaopi <382215239@qq.com>
2020-10-12 15:45:54 +0800

    Update 072_Bcdadd4.v
    a new solution by @Lysias-1998

--- 2_Circuits/072_Bcdadd4.v
+++ 2_Circuits/072_Bcdadd4.v
@@ -10,8 +10,35 @@ module top_module(
     generate
         genvar i;
         for(i = 4; i < 16; i = i + 4) begin:adder
-            bcd_fadd fadd(.a(a[i+3:i]), .b(b[i+3:i]), .cin(cout_tmp[i-4]), .cout(cout_tmp[i]), .sum(sum[i+3:i]));
+            bcd_fadd fadd(.a(a[i + 3:i]), .b(b[i + 3:i]), .cin(cout_tmp[i-4]), .cout(cout_tmp[i]), .sum(sum[i + 3:i]));
         end
     endgenerate
 
 endmodule
+
+// Below solution is designed by @Lysias-1998
+
+// module top_module( 
+//     input [15:0] a, b,
+//     input cin,
+//     output cout,
+//     output [15:0] sum );
+    
+//     wire [4:0] c;
+    
+//     assign c[0] = cin;
+//     assign cout = c[4];
+//     generate
+//         genvar i;
+//         for (i = 0; i < 4; i = i + 1) begin : adders
+//             bcd_fadd the_bcd_fadders ( 
+//                 .a(a[i*4+3 : i*4]), 
+//                 .b(b[i*4+3 : i*4]), 
+//                 .cin(c[i]), 
+//                 .cout(c[i+1]), 
+//                 .sum(sum[i*4+3 : i*4]) 
+//             );
+//         end
+//     endgenerate
+
+// endmodule
