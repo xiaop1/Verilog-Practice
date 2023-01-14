7d242b910eb3a3032f6553979b00054e0fb46263
JenniferKuo <meappp118@gmail.com>
2023-01-14 22:38:54 +0800

    add comment in 0-01

--- 0_Getting Started/01_Step_one.v
+++ 0_Getting Started/01_Step_one.v
@@ -1,5 +1,10 @@
 module top_module( output one );
 
 // Insert your code here
-    assign one = 1'b1;
+    assign one = 1'b1; 
+    // 1: 32-bit width, i.e. 32'b00000000_00000000_00000000_00000001
+    // 1'b1: 1-bit width
+    // see discussion in stackoverflow: https://stackoverflow.com/questions/59099852/difference-between-1-and-1b1-in-verilog
+    
+    
 endmodule
--- 3_Verification Reading simulations/166_Sim:circuit1.v
+++
@@ -1,8 +0,0 @@
-module top_module (
-    input a,
-    input b,
-    output q );//
-
-    assign q = a && b; // Fix me
-
-endmodule
