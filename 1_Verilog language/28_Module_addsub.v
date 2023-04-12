258ca6c05c49b546a63c573bc1bac48469adb9b1
Hai Zhu <35182391+cocolato@users.noreply.github.com>
2023-04-12 07:29:54 +0000

    Update 28_Module_addsub

--- 1_Verilog language/28_Module_addsub.v
+++ 1_Verilog language/28_Module_addsub.v
@@ -2,7 +2,7 @@ module top_module(
     input [31:0] a,
     input [31:0] b,
     input sub,
-    output [31:0] result
+    output [31:0] sum
 );
 
     wire cout1, cout2;
@@ -19,6 +19,6 @@ module top_module(
     add16 instance1(.a(a[15:0]), .b(b_xor[15:0]), .cin(sub), .cout(cout1), .sum(sum1));
     add16 instance2(.a(a[31:16]), .b(b_xor[31:16]), .cin(cout1), .cout(cout2), .sum(sum2));
     
-    assign result = {sum2, sum1};
+    assign sum = {sum2, sum1};
     
 endmodule
